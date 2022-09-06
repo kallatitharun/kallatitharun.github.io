0e3fc9653a963ddaa82d169a782b5d2c7b32532d
kallatitharun <44728862+kallatitharun@users.noreply.github.com>
2022-09-05 23:14:00 -0700

    Major Update using nav bar and padding of images

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def solution(n):
+    def digSum(x):
+        temp = 0
+        while (x != 0):
+            temp = temp+ x % 10
+            x = x // 10
+        return temp
+    flag=0
+    var=n
+    t=2*digSum(n)
+    while 1:
+        var+=1
+        if digSum(var)==t:
+            break
+    return var
+print(solution(500))
